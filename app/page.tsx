25dc2d7ce9664ce0fa9cb47df6e723b3c296246e
Halldór <halldor.hrafn.reynisson@gmail.com>
2023-11-30 20:09:54 +0000

    Tiny change to the index page

--- app/page.tsx
+++ app/page.tsx
@@ -3,11 +3,7 @@ import Navbar from '@/components/Navbar'
 export default async function Index() {
   return (
     <div className="flex-1 w-full flex flex-col gap-20 items-center">
-      <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
-        <div className="w-full flex justify-between items-center text-sm">
-          <Navbar />
-        </div>
-      </nav>
+        <Navbar />
       <main className="flex-1 flex flex-col items-center justify-center">
         <h1 className="text-4xl font-bold text-center text-foreground">
           I code about one line at a time.
