7a5c87430a45a98ecf35f3aa7ce995a9ca1a5221
Halldór <halldor.hrafn.reynisson@gmail.com>
2023-12-07 16:48:53 +0000

    Removed /post/create page and placed all the logic into the /post page

--- app/post/page.tsx
+++ app/post/page.tsx
@@ -1,6 +1,7 @@
 import Navbar from "@/components/Navbar";
 import { createClient } from "@/utils/supabase/server";
 import { cookies } from 'next/headers'
+import { redirect } from "next/navigation";
 
 export default async function Page() {
   const cookieStore = cookies()
@@ -11,18 +12,58 @@ export default async function Page() {
     .select("*")
     .order("created_at", { ascending: false });
 
+  const createPost = async (formData: FormData) => {
+    "use server"
+
+    const cookieStore = cookies();
+    const supabase = createClient(cookieStore);
+
+    const userId = (await supabase.auth.getUser()).data?.user?.id;
+
+    const title = formData.get("title") as string;
+    const content = formData.get("content") as string;
+
+    const { error } = await supabase.from("posts").insert([
+      {
+        title,
+        content,
+        author_id: userId,
+      },
+    ]);
+
+    return redirect("/post");
+  }
+
   return (
     <div className="flex-1 w-full flex flex-col gap-20 items-center">
       <Navbar />
       <main className="w-3/4 mx-auto">
-        <div className="p-10">
-          <a href="/post/create" className="inline-block px-4 py-2 text-white bg-blue-500 rounded-md hover:bg-blue-600">
-            Create Post
-          </a>
+        <div className="flex-1 w-full flex flex-col gap-20 items-center m-4">
+          <form action={createPost} className="max-w-md mx-auto bg-white rounded-lg shadow-md p-8">
+            <input
+              type="text"
+              placeholder="Title"
+              name="title"
+              className="w-full px-4 py-2 border border-gray-300 rounded-md mb-4 focus:outline-none focus:ring-2 focus:ring-blue-500"
+            />
+            <textarea
+              placeholder="Content"
+              name="content"
+              className="w-full px-4 py-2 border border-gray-300 rounded-md mb-4 focus:outline-none focus:ring-2 focus:ring-blue-500"
+            ></textarea>
+            <button
+              type="submit"
+              className="px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600"
+            >
+              Submit
+            </button>
+          </form>
         </div>
         {posts?.map((post, index) => (
           <div key={index} className="bg-white rounded-md p-4 mb-4">
-            <h3 className="text-xl font-bold mb-2">{post.title}</h3>
+            <h3 className="text-xl font-bold mb-2">
+              <a href={`/post/${post.id}`}>{post.title}</a>
+            </h3>
             <p className="text-gray-500">{post.content}</p>
             <a href={`/profile/${post.author_id}`} className="text-blue-500 hover:underline">Author</a>
           </div>
