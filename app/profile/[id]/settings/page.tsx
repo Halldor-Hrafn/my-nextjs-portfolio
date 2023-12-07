134f3d48c7329603a1b9008db886ee6c6f0d0ee4
Halldór <halldor.hrafn.reynisson@gmail.com>
2023-12-07 16:43:27 +0000

    Worked more on the changing of passwords while the user is logged in.

--- app/profile/[id]/settings/page.tsx
+++ app/profile/[id]/settings/page.tsx
@@ -1,8 +1,16 @@
 import Navbar from "@/components/Navbar";
 import { createClient } from "@/utils/supabase/server";
 import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
 
-export default async function Page() {
+export default async function Page({
+  searchParams,
+}: {
+  searchParams: { 
+    passwordErrorMessage: string,
+    passwordSuccessMessage: string
+   };
+}) {
   const cookieStore = cookies();
   const supabase = createClient(cookieStore);
 
@@ -20,9 +28,7 @@ export default async function Page() {
       .from("profiles")
       .update({ user_name: displayName })
       .eq("auth_id", userId);
-
-    console.log(error);
-  }
+  };
 
   const handlePasswordReset = async (formData: FormData) => {
     "use server";
@@ -30,21 +36,32 @@ export default async function Page() {
     const cookiesStore = cookies();
     const supabase = createClient(cookiesStore);
 
-    const oldPassword = formData.get("oldPassword") as string;
+    const userId = (await supabase.auth.getUser()).data?.user?.id;
+
     const newPassword = formData.get("newPassword") as string;
+    const newPasswordVerify = formData.get("newPasswordVerify") as string;
+
+    if (newPassword !== newPasswordVerify) {
+      return redirect(`/profile/${userId}/settings?passwordErrorMessage=Passwords do not match`);
+    }
 
     const { error } = await supabase.auth.updateUser({
-      password: newPassword,
+      password: newPasswordVerify,
     });
+
+    return redirect(`/profile/${userId}/settings?passwordSuccessMessage=Password changed`)
   };
 
   return (
     <div className="flex-1 w-full flex flex-col gap-20 items-center">
       <Navbar />
       <main className="w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-        <div className="col-span-1 bg-white rounded-lg shadow-md p-8">
+        <div className="col-span-1 bg-white rounded-lg shadow-md p-8 m-2">
           <h2 className="text-2xl font-bold mb-4">Display Name</h2>
-          <form action={handleDisplayNameChange} className="flex flex-col gap-4">
+          <form
+            action={handleDisplayNameChange}
+            className="flex flex-col gap-4"
+          >
             <input
               type="text"
               placeholder="New Display Name"
@@ -59,19 +76,19 @@ export default async function Page() {
             </button>
           </form>
         </div>
-        <div className="col-span-1 bg-white rounded-lg shadow-md p-8">
+        <div className="col-span-1 bg-white rounded-lg shadow-md p-8 m-2">
           <h2 className="text-2xl font-bold mb-4">Change Password</h2>
           <form action={handlePasswordReset} className="flex flex-col gap-4">
             <input
               type="password"
-              placeholder="Old Password"
-              name="oldPassword"
+              placeholder="New Password"
+              name="newPassword"
               className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
             />
             <input
               type="password"
-              placeholder="New Password"
-              name="newPassword"
+              placeholder="New Password (Verify)"
+              name="newPasswordVerify"
               className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
             />
             <button
@@ -80,6 +97,16 @@ export default async function Page() {
             >
               Change
             </button>
+            {searchParams?.passwordErrorMessage && (
+              <p className="mt-4 p-4 bg-red-100 text-red-900 text-center">
+                {searchParams.passwordErrorMessage}
+              </p>
+            )}
+            {searchParams?.passwordSuccessMessage && (
+              <p className="mt-4 p-4 bg-green-100 text-green-900 text-center">
+                {searchParams.passwordSuccessMessage}
+              </p>
+            )}
           </form>
         </div>
       </main>
