a55c8624ea730f9b0681f7228a5f2cf10a3bac6e
Halldór <halldor.hrafn.reynisson@gmail.com>
2023-12-07 18:21:36 +0000

    Small change to how the image path is stored.

--- app/post/page.tsx
+++ app/post/page.tsx
@@ -33,7 +33,7 @@ export default async function Page() {
     ])
     .select();
 
-    const imagePath = `post/${post![0].id}`
+    const imagePath = `post/${post![0].id}/${image.name}`
 
     const { data, error } = await supabase
       .from("post-images")
