f568b94e90504814f264aa49b39e79590a9c7770
Halldór <halldor.hrafn.reynisson@gmail.com>
2023-12-13 14:53:16 +0000

    Added a feature request/bug report section to the about me page. Should probably move it eventually

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,7 +1,43 @@
 import React from 'react';
 import Navbar from '@/components/Navbar';
+import { redirect } from 'next/navigation';
+
+import { Resend } from 'resend';
 
 export default async function Page() {
+  const sendRequest = async (formData: FormData) => {
+    "use server"
+
+    const resend = new Resend(process.env.RESEND_API_KEY!);
+
+    const name = formData.get("name") as string;
+    const type = formData.get("type") as string;
+    const description = formData.get("description") as string;
+
+    const email = process.env.EMAIL!;
+
+    let typeString = "";
+
+    if (type === "feature") {
+      typeString = "Feature Request"
+    } else {
+      typeString = "Bug Report"
+    }
+
+    try {
+      await resend.emails.send({
+        from: 'Mr. Potts <portfolio@pottss.dev>',
+        to: [email],
+        subject: typeString,
+        text: `Name: ${name}\nType: ${type}\nDescription: ${description}`,
+      })
+    } catch (error) {
+      console.log(error)
+    }
+
+    return redirect("/about");
+  }
+
   return (
     <div className="flex-1 w-full flex flex-col gap-20 items-center">
       <Navbar />
@@ -33,6 +69,55 @@ export default async function Page() {
             that I'm good enough to work for a company yet. I am mostly exploring backend development aswell as low-level programming.
           </p>
         </div>
+      </main>
+      <main className="flex-1 flex flex-col items-center justify-center h-full">
+        <form action={sendRequest} className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+          <h2 className="text-2xl font-bold mb-6">Submit a Feature Request/Bug Report</h2>
+          <div className="mb-4">
+            <label htmlFor="name" className="block text-gray-700 font-bold mb-2">
+              Name:
+            </label>
+            <input
+              type="text"
+              id="name"
+              name="name"
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              required
+            />
+          </div>
+          <div className="mb-4">
+            <label htmlFor="type" className="block text-gray-700 font-bold mb-2">
+              Type:
+            </label>
+            <select
+              id="type"
+              name="type"
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              required
+            >
+              <option value="feature">Feature Request</option>
+              <option value="bug">Bug Report</option>
+            </select>
+          </div>
+          <div className="mb-4">
+            <label htmlFor="description" className="block text-gray-700 font-bold mb-2">
+              Description:
+            </label>
+            <textarea
+              id="description"
+              name="description"
+              rows="4"
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              required
+            ></textarea>
+          </div>
+          <button
+            type="submit"
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+          >
+            Submit
+          </button>
+        </form>
       </main>
     </div>
   );
