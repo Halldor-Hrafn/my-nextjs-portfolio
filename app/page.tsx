3d7d5c1defda9cacf720a2a56cc3d37dfcb8cbe2
Halldór <halldor.hrafn.reynisson@gmail.com>
2023-12-17 17:01:38 +0000

    Final attempt to see if signin using Github works

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,32 @@
-import Navbar from '@/components/Navbar'
+import Navbar from "@/components/Navbar";
+import { createClient } from '@/utils/supabase/server'
+import { cookies } from 'next/headers'
 
-export default async function Index() {
+export default async function Index({
+  searchParams,
+}: {
+  searchParams: { code: string };
+}) {
+  const code = searchParams.code;
+
+  if (code) {
+    const cookiesStore = cookies();
+    const supabase = createClient(cookiesStore);
+    await supabase.auth.exchangeCodeForSession(code);
+  }
+
   return (
     <div className="flex-1 w-full flex flex-col gap-20 items-center">
-        <Navbar />
+      <Navbar />
       <main className="flex-1 flex flex-col items-center justify-center">
         <h1 className="text-4xl font-bold text-center text-foreground">
           I code about one line at a time.
         </h1>
         <p className="text-xl text-center text-foreground">
-          I am a wanabe developer who gets side-tracked from their projects 100% of the time.
+          I am a wanabe developer who gets side-tracked from their projects 100%
+          of the time.
         </p>
       </main>
     </div>
-  )
+  );
 }
